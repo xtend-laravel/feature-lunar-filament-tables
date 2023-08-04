42cd73ff1e4ad9c4515ce3adc97cc29cd28d8179
Adam Lee <adam@codelabx.ltd>
2023-08-04 14:11:02 +0100

    Form model support for tables

--- resources/views/livewire/components/tables/base-table.blade.php
+++ resources/views/livewire/components/tables/base-table.blade.php
@@ -1,3 +1,11 @@
 <div>
-  {{ $this->table }}
+    {{ $this->table }}
+
+    <div class="ui-slideover-forms">
+        @each('adminhub::partials.ui.slideover-wizard', $this->slideoverForms ?? [], 'slideoverForm')
+    </div>
+
+    <div class="ui-modal-forms">
+        @each('adminhub::partials.ui.modal', $this->modalForms ?? [], 'modalForm')
+    </div>
 </div>
